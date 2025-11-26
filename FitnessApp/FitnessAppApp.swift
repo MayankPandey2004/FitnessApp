2c905705a3f8c17045d3c16092e7fd68fd4e1c9b
MayankPandey2004 <mpandey2004@gmail.com>
2025-11-27 04:55:56 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Mayank Pandey on 27/11/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
